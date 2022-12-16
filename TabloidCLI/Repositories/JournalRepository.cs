6abb472f35271d77e00fc6f99e6dd4a4b5a1be5e
Rebekah Taylor <rebekahtdesigns@gmail.com>
2022-12-16 07:38:12 -0800

    added journal info

---
+++ TabloidCLI/Repositories/JournalRepository.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.Data.SqlClient;
+using TabloidCLI.Models;
+using TabloidCLI.Repositories;
+
+namespace TabloidCLI
+{
+    public class JournalRepository : DatabaseConnector, IRepository<Journal>
+    {
+        public JournalRepository(string connectionString) : base(connectionString) { }
+
+        public List<Journal> GetAll()
+        {
+
+            using (SqlConnection conn = Connection)
+            {
+                conn.Open();
+                using (SqlCommand cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = @"SELECT 
+                                        Title,
+                                        Content,
+                                        CreateDateTime 
+                                        FROM Journal";
+                    List<Journal> journals = new List<Journal>();
+
+                    SqlDataReader reader = cmd.ExecuteReader();
+                    while (reader.Read())
+                    {
+                        Journal journal = new Journal()
+                        {
+                            Title = reader.GetString(reader.GetOrdinal("Title")),
+                            Content = reader.GetString(reader.GetOrdinal("Content")),
+                            CreateDateTime = reader.GetDateTime(reader.GetOrdinal("CreateDateTime"))
+                        };
+                        journals.Add(journal);
+                    }
+
+                    reader.Close();
+
+                    return journals;
+                }
+            }
+        }
+
+        public Journal Get(int id)
+        {
+            using (SqlConnection conn = Connection)
+            {
+                conn.Open();
+                using (SqlCommand cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = @"SELECT
+                                        id, 
+                                        Title, 
+                                        Content, 
+                                        CreateDateTime 
+                                        FROM Journal
+                                        WHERE id = @id";
+
+                    cmd.Parameters.AddWithValue("@id", id);
+
+                    Journal journal = null;
+
+                    SqlDataReader reader = cmd.ExecuteReader();
+                    while (reader.Read())
+                    {
+                        if (journal == null)
+                        {
+                            journal = new Journal()
+                            {
+                                Id = reader.GetInt32(reader.GetOrdinal("Id")),
+                                Title = reader.GetString(reader.GetOrdinal("Title")),
+                                Content = reader.GetString(reader.GetOrdinal("Content")),
+                                CreateDateTime = reader.GetDateTime(reader.GetOrdinal("CreateDateTime"))
+                            };
+                        }
+                    }
+
+                    reader.Close();
+
+                    return journal;
+                }
+            }
+        }
+
+            public void Insert(Journal journal)
+            {
+                using (SqlConnection conn = Connection)
+                {
+                    conn.Open();
+                    using (SqlCommand cmd = conn.CreateCommand())
+                    {
+                        cmd.CommandText = @"INSERT INTO Journal (Title, Content, CreateDateTime )
+                                                     VALUES (@title, @content, @create )";
+                        cmd.Parameters.AddWithValue("@title", journal.Title);
+                        cmd.Parameters.AddWithValue("@content", journal.Content);
+                        cmd.Parameters.AddWithValue("@create", DateTime.Now);
+                    cmd.ExecuteNonQuery();
+                    }
+                }
+            }
+
+            public void Update(Journal journal)
+            {
+                throw new NotImplementedException();
+            }
+
+            public void Delete(int id)
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+
+}
--- TabloidCLI/UserInterfaceManagers/MainMenuManager.cs
+++ TabloidCLI/UserInterfaceManagers/MainMenuManager.cs
@@ -25,12 +25,13 @@ namespace TabloidCLI.UserInterfaceManagers
             string choice = Console.ReadLine();
             switch (choice)
             {
-                case "1": throw new NotImplementedException();
+                case "1": return new JournalManager(this, CONNECTION_STRING);
                 case "2": throw new NotImplementedException();
                 case "3": return new AuthorManager(this, CONNECTION_STRING);
                 case "4": throw new NotImplementedException();
                 case "5": return new TagManager(this, CONNECTION_STRING);
                 case "6": return new SearchManager(this, CONNECTION_STRING);
+                case "7": return new JournalManager(this, CONNECTION_STRING);
                 case "0":
                     Console.WriteLine("Good bye");
                     return null;
